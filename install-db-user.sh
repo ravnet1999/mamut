a5e54453e7453696249eadf80e01b5fbe32ef850
Piotr Jankowski <p.jankowski@imagho.pl>
2020-09-16 13:52:57 +0200

    Fixes installation script

--- install-db-user.sh
+++ install-db-user.sh
@@ -2,6 +2,7 @@
 
 cp ./src/backend/config/database.json.example ./src/backend/config/database.json
 cp ./src/parser/config/database.json.example ./src/parser/config/database.json
+cp ./src/translator/config/database.json.example ./src/translator/config/database.json
 cp ./src/mongodb/scripts/add_user.js.example ./src/mongodb/scripts/add_user.js
 
 sudo docker-compose -f docker-compose.$1.yml exec -T mamut-app-db /bin/sh -c "/usr/bin/mongo -u $2 --password $3 < /scripts/add_user.js"
