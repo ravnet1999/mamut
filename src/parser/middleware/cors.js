9c333098a450cddd26c6144e3317e86054206118
Piotr Jankowski <p.jankowski@imagho.pl>
2020-09-16 13:46:07 +0200

    Modifies CORS middleware

--- src/parser/middleware/cors.js
+++ src/parser/middleware/cors.js
@@ -1,7 +1,7 @@
 const cors = (req, res, next) => {
     res.setHeader('Access-Control-Allow-Methods', 'PUT, OPTIONS, GET, POST');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8316');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8501');
     res.setHeader('Access-Control-Allow-Credentials', 'true');
     res.setHeader('Content-Type', 'application/json');
     res.setHeader('Accept', 'application/json');
