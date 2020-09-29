687b43e6962470bc56daeff91db467701cbf1816
Piotr Jankowski <p.jankowski@imagho.pl>
2020-09-29 11:00:08 +0200

    Modifies the install script

--- install-db-user.sh
+++ install-db-user.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
 cp ./src/backend/config/database.json.example ./src/backend/config/database.json
+cp ./src/backend/middleware/cors.js.example ./src/backend/middleware/cors.js
+cp ./src/frontend/src/Config/appConfig.json.example ./src/frontend/src/Config/appConfig.json
 cp ./src/parser/config/database.json.example ./src/parser/config/database.json
+cp ./src/parser/middleware/cors.js.example ./src/parser/middleware/cors.js
 cp ./src/translator/config/database.json.example ./src/translator/config/database.json
 cp ./src/mongodb/scripts/add_user.js.example ./src/mongodb/scripts/add_user.js
 
