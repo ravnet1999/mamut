4491a65005be0e8f3b7e7f58a01979591b8139db
Piotr Jankowski <p.jankowski@imagho.pl>
2020-10-06 14:53:37 +0200

    Fixes frontend installation script

--- install-frontend.sh
+++ install-frontend.sh
@@ -2,6 +2,9 @@
 
 sudo docker-compose -f docker-compose.dev.yml exec $1 /bin/sh /app/scripts/build-frontend.sh
 
+mkdir src/backend/public/admin
+mkdir src/parser/public/admin
+
 sudo rm -rf src/backend/public/admin/*
 rsync -avz src/frontend/build/* src/backend/public/admin/
 sudo chown -R $2:$3 src/backend/public/admin/*
