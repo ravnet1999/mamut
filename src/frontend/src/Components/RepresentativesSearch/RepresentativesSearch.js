8209284b69b44df2837a3746adb314b0bd3bb158
Alicja Konopka <alicja.konopka@wp.pl>
2022-01-20 15:36:54 +0100

    new empty search representatives component

---
+++ src/frontend/src/Components/RepresentativesSearch/RepresentativesSearch.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './RepresentativesSearch.css';
+
+const RepresentativesSearch = (props) => {
+    return (
+        <p>Tutaj będzie wyszukiwarka</p>
+    );
+}
+
+export default RepresentativesSearch;
