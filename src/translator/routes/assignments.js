c086f3911f5c4459dbebfb82bd1eae00b3162e9d
Alicja Konopka <alicja.konopka@wp.pl>
2022-01-24 16:05:46 +0100

    code simplification

--- src/translator/routes/assignments.js
+++ src/translator/routes/assignments.js
@@ -37,80 +37,86 @@ router.get('/:operatorId', (req, res, next) => {
     // });
 });
 
-router.get('/:clientIds/representatives', (req, res, next) => {
-    let clientIds = req.params.clientIds.split(',');
+router.get('/:clientIds/representatives', async (req, res, next) =>  {
+  let clientIds = req.params.clientIds.split(',');
 
-    let allTasks = [];
-    let allTasksIds = [];
-    let allOperatorIds = [];
+  let allTasks = [];
+  let allTasksIds = [];
+  let allOperatorIds = [];
 
-    taskService.find(9999999, 0, 'id', 'DESC', '`status` = \'open\'').then((tasks) => {
-        let operatorIds = tasks.map((task) => {
-            return task.informatyk;
-        });
-        allOperatorIds = operatorIds.filter((operatorId, index, self) => {
-            return self.indexOf(operatorId) === index;
-        });
-        allTasksIds = tasks.map((task) => {
-            return task.id;
-        });
-        allTasks = tasks;
-        return episodeService.find(99999999, 0, 'id', 'DESC', 'id_zgloszenia IN (' + allTasksIds.join(',') + ')');
-    }).then((episodes) => {
-        allTasks.map((task) => {
-            task.lastEpisode = episodes.filter((episode) => {
-                return episode.id_zgloszenia == task.id;
-            })[0];
+  try {
+    let tasks = await taskService.find(9999999, 0, 'id', 'DESC', '`status` = \'open\'');  
 
-            return task;
-        });
-        return operatorService.findById(allOperatorIds);
-    }).then((operators) => {
-        allTasks = allTasks.map((task) => {
-            task.operator = {};
-
-            let taskOperators = operators.filter((operator) => {
-                return operator.id == task.informatyk;
-            });
-
-            if(taskOperators.length > 0) {
-                delete taskOperators[0].login;
-                delete taskOperators[0].haslo;
-                task.operator = taskOperators[0];
-            }
-
-            return task;
-        })
-        return userService.findByClientId(clientIds);
-    }).then((users) => {
-        let usersWithTasks = users.map((user) => {
-          user = charset.translateIn(user);
-          user.activeTasks = allTasks.filter((task) => {
-              task = charset.translateIn(task);
-              return task.id_klienta == user.id_klienta && task.id_zglaszajacy == user.id;
-          });
-          console.log(user.activeTasks, 'active tasks');
-          return user;
-        });
-
-        response(res, false, ['Pomyślnie pobrano reprezentantów klienta.'], usersWithTasks);
-        return;
-    }).catch((err) => {
-        console.log(err);
-        response(res, false, ['Coś poszło nie tak podczas próby pobrania aktywnych zadań reprezentantów', JSON.stringify(err)], []);
-        return;
+    let operatorIds = tasks.map((task) => {
+      return task.informatyk;
     });
-    // userService.findByClientId(clientIds).then((clients) => {
-    //     clients = clients.map((client) => {
-    //         client = charset.translateIn(client);
-    //         return client;
-    //     });
-    //     response(res, false, ['Pomyślnie pobrano reprezentantów klienta.'], clients);
-    //     return;
-    // }).catch((err) => {
-    //     response(res, false, ['Coś poszło nie tak podczas próby pobrania reprezentantów klienta.', JSON.stringify(err)], []);
-    //     return;
-    // });
+    allOperatorIds = operatorIds.filter((operatorId, index, self) => {
+        return self.indexOf(operatorId) === index;
+    });
+    allTasksIds = tasks.map((task) => {
+        return task.id;
+    });
+    allTasks = tasks;
+    
+    let episodes = await episodeService.find(99999999, 0, 'id', 'DESC', 'id_zgloszenia IN (' + allTasksIds.join(',') + ')');
+    
+    allTasks.map((task) => {
+      task.lastEpisode = episodes.filter((episode) => {
+          return episode.id_zgloszenia == task.id;
+      })[0];
+
+      return task;
+    });
+
+    let operators = await operatorService.findById(allOperatorIds);
+    
+    allTasks = allTasks.map((task) => {
+      task.operator = {};
+
+      let taskOperators = operators.filter((operator) => {
+          return operator.id == task.informatyk;
+      });
+
+      if(taskOperators.length > 0) {
+          delete taskOperators[0].login;
+          delete taskOperators[0].haslo;
+          task.operator = taskOperators[0];
+      }
+
+      return task;
+    });
+
+    let users = await userService.findByClientId(clientIds);
+    
+    let usersWithTasks = users.map((user) => {
+      user = charset.translateIn(user);
+      user.activeTasks = allTasks.filter((task) => {
+          task = charset.translateIn(task);
+          return task.id_klienta == user.id_klienta && task.id_zglaszajacy == user.id;
+      });
+      console.log(user.activeTasks, 'active tasks');
+      return user;
+    });
+
+    response(res, false, ['Pomyślnie pobrano reprezentantów klienta.'], usersWithTasks);
+    return;
+        
+  } catch(err) {
+      console.log(err);
+      response(res, false, ['Coś poszło nie tak podczas próby pobrania aktywnych zadań reprezentantów', JSON.stringify(err)], []);
+      return;
+  };
+  // userService.findByClientId(clientIds).then((clients) => {
+  //     clients = clients.map((client) => {
+  //         client = charset.translateIn(client);
+  //         return client;
+  //     });
+  //     response(res, false, ['Pomyślnie pobrano reprezentantów klienta.'], clients);
+  //     return;
+  // }).catch((err) => {
+  //     response(res, false, ['Coś poszło nie tak podczas próby pobrania reprezentantów klienta.', JSON.stringify(err)], []);
+  //     return;
+  // });
 });
 
 module.exports = router;
