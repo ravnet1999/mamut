eb5e1b23170ca279709b7681b509a0a0c056b90d
Alicja Konopka <alicja.konopka@wp.pl>
2022-01-25 11:58:59 +0100

    limiting search results to active representatives

--- src/translator/src/services/UserClientService.js
+++ src/translator/src/services/UserClientService.js
@@ -14,8 +14,9 @@ class UserClientService {
         connection.query('SELECT `uzytkownicy`.`id`, `tel_komorkowy`, `numer_wewnetrzny`, `imie`,`nazwisko`, id_klienta, sl_klientow.nazwa FROM `' 
           + this.usersTableName + '` join ' + this.clientsTableName 
           + ' on uzytkownicy.id_klienta=sl_klientow.id'
-          + ' WHERE REPLACE(REPLACE(REPLACE(REPLACE(`tel_komorkowy`,"-","")," ", ""),  "tel.", ""), "kom.", "") LIKE ?'
-          + ' OR REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(`numer_wewnetrzny`,"-","")," ", ""), "tel.:", ""), "w.", ""), "(", ""), ")", "") LIKE ?',
+          + ' WHERE (REPLACE(REPLACE(REPLACE(REPLACE(`tel_komorkowy`,"-","")," ", ""),  "tel.", ""), "kom.", "") LIKE ?'
+          + ' OR REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(`numer_wewnetrzny`,"-","")," ", ""), "tel.:", ""), "w.", ""), "(", ""), ")", "") LIKE ?)'
+          + ' AND `uzytkownicy`.`aktywny`=\'on\'',
           ["%" + phoneNumber + "%", "%" + phoneNumber + "%"], (err, results, fields) => {
             if(err) {
                 reject(err);
