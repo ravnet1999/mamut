111ae6c9634dba7e187ab3a5c37c6fe8293ac76f
Alicja Konopka <alicja.konopka@wp.pl>
2022-01-25 16:07:58 +0100

    user handler

---
+++ src/frontend/src/Handlers/UserHandler.js
@@ -0,0 +1,30 @@
+import axios from 'axios';
+import parseResponse from './ApiParser';
+import appConfig from '../Config/appConfig.json';
+
+const UserHandler = {
+    getWithTasks: (repId) => {
+        return new Promise((resolve, reject) => {
+            axios.get(`${appConfig.URLs.domain}/${appConfig.URLs.representatives}/${repId}`, {
+                withCredentials: true
+            }).then((response) => {
+                parseResponse(response).then((response) => {
+                    resolve(response);
+                    return;
+                }).catch((err) => {
+                    reject(err);
+                    return;
+                });
+            }).catch((err) => {
+                reject({
+                    error: true,
+                    messages: ['Wystąpił problem z połączeniem z serwerem.', JSON.stringify(err)],
+                    resources: []
+                });
+                return;
+            });
+        });
+    },
+}
+
+export default UserHandler;
