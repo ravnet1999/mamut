01b77aa45d0227b80cd0495c56b1d8a360dc634e
Alicja Konopka <alicja.konopka@wp.pl>
2022-01-25 10:26:08 +0100

    disables mails sending

--- src/backend/src/classess/Mailer.js
+++ src/backend/src/classess/Mailer.js
@@ -8,13 +8,13 @@ class Mailer {
     }
 
     send = (from, to, subject, text, html) => {
-        this.transporter.sendMail({
-            from: from,
-            to: to,
-            subject: subject,
-            text: text,
-            html: html
-        });
+        // this.transporter.sendMail({
+        //     from: from,
+        //     to: to,
+        //     subject: subject,
+        //     text: text,
+        //     html: html
+        // });
     }
 
     getConfig = () => {
