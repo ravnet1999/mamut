8d2a0974a75080a0e805c55c9d369c92451eaeea
Alicja Konopka <alicja.konopka@wp.pl>
2022-01-26 18:46:46 +0100

    representative search for phone numbers longer then 2 digits

--- src/translator/src/services/UserClientService.js
+++ src/translator/src/services/UserClientService.js
@@ -11,21 +11,26 @@ class UserClientService {
     findByPhoneNumber = (phoneNumber) => {
       return new Promise((resolve, reject) => {
         phoneNumber = phoneNumber.replace(/\D/g,'');
-        connection.query('SELECT `uzytkownicy`.`id`, `tel_komorkowy`, `numer_wewnetrzny`, `imie`,`nazwisko`, id_klienta, sl_klientow.nazwa FROM `' 
-          + this.usersTableName + '` join ' + this.clientsTableName 
-          + ' on uzytkownicy.id_klienta=sl_klientow.id'
-          + ' WHERE (REPLACE(REPLACE(REPLACE(REPLACE(`tel_komorkowy`,"-","")," ", ""),  "tel.", ""), "kom.", "") LIKE ?'
-          + ' OR REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(`numer_wewnetrzny`,"-","")," ", ""), "tel.:", ""), "w.", ""), "(", ""), ")", "") LIKE ?)'
-          + ' AND `uzytkownicy`.`aktywny`=\'on\'',
-          ["%" + phoneNumber + "%", "%" + phoneNumber + "%"], (err, results, fields) => {
-            if(err) {
-                reject(err);
-                return;
-            }
 
-            resolve(results);
-            return;
-        });
+        if(phoneNumber.length>2) {
+          connection.query('SELECT `uzytkownicy`.`id`, `tel_komorkowy`, `numer_wewnetrzny`, `imie`,`nazwisko`, id_klienta, sl_klientow.nazwa FROM `' 
+            + this.usersTableName + '` join ' + this.clientsTableName 
+            + ' on uzytkownicy.id_klienta=sl_klientow.id'
+            + ' WHERE (REPLACE(REPLACE(REPLACE(REPLACE(`tel_komorkowy`,"-","")," ", ""),  "tel.", ""), "kom.", "") LIKE ?'
+            + ' OR REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(`numer_wewnetrzny`,"-","")," ", ""), "tel.:", ""), "w.", ""), "(", ""), ")", "") LIKE ?)'
+            + ' AND `uzytkownicy`.`aktywny`=\'on\'',
+            ["%" + phoneNumber + "%", "%" + phoneNumber + "%"], (err, results, fields) => {
+              if(err) {
+                  reject(err);
+                  return;
+              }
+
+              resolve(results);
+              return;
+          });
+        } else {
+          resolve([]);
+        }
       });    
     }
 }
