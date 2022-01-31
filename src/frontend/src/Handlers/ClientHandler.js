f81a217c26dafe09b15164c94632c0d900224858
Alicja Konopka <alicja.konopka@wp.pl>
2022-01-31 11:28:08 +0100

    moving function for sorting clients to client handler

--- src/frontend/src/Handlers/ClientHandler.js
+++ src/frontend/src/Handlers/ClientHandler.js
@@ -2,6 +2,29 @@ import axios from 'axios';
 import parseResponse from '../Handlers/ApiParser';
 import appConfig from '../Config/appConfig.json';
 
+const getUnsortedRepresentatives = (clientId) => {
+  return new Promise((resolve, reject) => {
+      axios.get(`${appConfig.URLs.domain}/${appConfig.URLs.representatives}/findByClientIds/${clientId}`, {
+          withCredentials: true
+      }).then((response) => {
+          parseResponse(response).then((response) => {
+              resolve(response);
+              return;
+          }).catch((err) => {
+              reject(err);
+              return;
+          });
+      }).catch((err) => {
+          reject({
+              error: true,
+              messages: ['Wystąpił problem z połączeniem z serwerem.', JSON.stringify(err)],
+              resources: []
+          });
+          return;
+      });
+  });
+};
+
 const ClientHandler = {
     getClients: () => {
         return new Promise((resolve, reject) => {
@@ -26,27 +49,16 @@ const ClientHandler = {
             });
         });
     },
-    getRepresentatives: (clientId) => {
-        return new Promise((resolve, reject) => {
-            axios.get(`${appConfig.URLs.domain}/${appConfig.URLs.representatives}/findByClientIds/${clientId}`, {
-                withCredentials: true
-            }).then((response) => {
-                parseResponse(response).then((response) => {
-                    resolve(response);
-                    return;
-                }).catch((err) => {
-                    reject(err);
-                    return;
-                });
-            }).catch((err) => {
-                reject({
-                    error: true,
-                    messages: ['Wystąpił problem z połączeniem z serwerem.', JSON.stringify(err)],
-                    resources: []
-                });
-                return;
-            });
-        });
+    getRepresentatives: getUnsortedRepresentatives,
+
+    getSortedRepresentatives: async (clientId) => {
+      const response = await getUnsortedRepresentatives(clientId);
+      const unsortedRepresentatives = response.resources;
+      const sortedRepresentatives = unsortedRepresentatives.sort((a, b) => {
+        return (a.imie + a.nazwisko) > (b.imie + b.nazwisko) ? 1 : ( (a.imie + a.nazwisko) < (b.imie + b.nazwisko) ? -1 : 0 );
+      });
+
+      return { resources: sortedRepresentatives };
     },
 
     getRepresentative: (repId) => {
