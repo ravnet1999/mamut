b59c4009cf517e1b8df2226cc5234fe8b51b0fb8
Alicja Konopka <alicja.konopka@wp.pl>
2022-01-31 11:00:00 +0100

    moving tasks related states from representatives component to task context

--- src/frontend/src/Contexts/TaskContext.js
+++ src/frontend/src/Contexts/TaskContext.js
@@ -5,6 +5,26 @@ export const TaskContext = createContext();
 
 const TaskContextProvider = ({children}) => {
   const [ taskStarted, setTaskStarted ] = useState(false);
+  const [ tasksVisible, setTasksVisible ] = useState(false);
+  const [ takeOverStarted, setTakeOverStarted ] = useState(false);
+  const [ viewedOperator, setViewedOperator ] = useState(null);
+  const [ viewedTaskList, setViewedTaskList ] = useState([]);
+  const [ activeTasksModal, setActiveTasksModal ] = useState({
+      title: '',
+      description: ''
+  });
+  const [ taskForTakeOver, setTaskForTakeOver ] = useState(null);
+  const [ takeOverModalVisible, setTakeOverModalVisible ] = useState(false);
+  const [ takeOverButtonDisabled, setTakeOverButtonDisabled ] = useState({
+      status: false
+  });
+  const [ takeOverModal, setTakeOverModal ] = useState({
+      title: '',
+      description: '',
+      buttons: []
+  });
+  const [ taskPreviewVisible, setTaskPreviewVisible ] = useState(false);
+  const [ previewedTask, setPreviewedTask ] = useState(null);
 
   const createTask = (selectedRepId, selectedClientId) => {
     setTaskStarted(true);
@@ -13,7 +33,7 @@ const TaskContextProvider = ({children}) => {
 
   return (
     <div>
-      <TaskContext.Provider value={{ taskStarted, createTask }} >
+      <TaskContext.Provider value={{ taskStarted, setTaskStarted, createTask, tasksVisible, setTasksVisible, takeOverStarted, setTakeOverStarted, viewedOperator, setViewedOperator, viewedTaskList, setViewedTaskList, activeTasksModal, setActiveTasksModal, taskForTakeOver, setTaskForTakeOver, takeOverModalVisible, setTakeOverModalVisible, takeOverButtonDisabled, setTakeOverButtonDisabled, takeOverModal, setTakeOverModal, taskPreviewVisible, setTaskPreviewVisible, previewedTask, setPreviewedTask }} >
         {children}
       </TaskContext.Provider>
     </div>
