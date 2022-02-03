419375bf94e3ff57ece631fddcbb4efcee80eeaa
Alicja Konopka <alicja.konopka@wp.pl>
2022-02-03 17:09:25 +0100

    separate page for displaying representatives search engine

--- src/frontend/src/Components/Header.js
+++ src/frontend/src/Components/Header.js
@@ -10,8 +10,8 @@ const Header = (props) => {
                     <Col>
                         <nav>
                             <ul>
-                                <li><Link to='/representatives-search' className={props.currentPage.includes('/representatives-search') || props.currentPage.includes('/representatives-search')  ? 'active' : ''}>Reprezentanci</Link></li>
-                                <li><Link to='/clients' className={props.currentPage.includes('/clients') || props.currentPage.includes('/representatives') &&  props.currentPage !== '/representatives-search' ? 'active' : ''}>Klienci</Link></li>
+                                <li><Link to='/representatives-search' className={props.currentPage.includes('/representatives-search') ? 'active' : ''}>Reprezentanci</Link></li>
+                                <li><Link to='/clients' className={props.currentPage.includes('/clients') || props.currentPage.includes('/representatives') &&  !props.currentPage.includes('/representatives-search') ? 'active' : ''}>Klienci</Link></li>
                                 <li><Link to='/tasks/general' className={props.currentPage.includes('/tasks/general') ? 'active' : ''}>ToDo ({props.generalTasksCount})</Link></li>
                                 <li><Link to='/tasks' className={props.currentPage.includes('/tasks') && !props.currentPage.includes('/tasks/general') ? 'active' : ''}>W trakcie</Link></li>
                                 <li><Link to='/logout'>Wyloguj</Link></li>
