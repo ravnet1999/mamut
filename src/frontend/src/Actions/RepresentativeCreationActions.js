aa7854d6301c99f17a615d070ccd4bb80e0df45f
Alicja Konopka <alicja.konopka@wp.pl>
2022-02-07 11:48:35 +0100

    moving state and state changes related code to reducer and context files

--- src/frontend/src/Actions/RepresentativeCreationActions.js
+++ src/frontend/src/Actions/RepresentativeCreationActions.js
@@ -9,3 +9,11 @@ export const showRepCreationFormModal = () => {
 export const updateRepCreationFormModal = (data) => {
   return { type: 'UPDATE_REP_CREATION_FORM_MODAL', data };
 };
+
+export const updateForm = (data) => {
+  return { type: 'UPDATE_FORM', data };
+};
+
+export const setResponse = (data) => {
+  return { type: 'SET_RESPONSE', data };
+};
--- src/frontend/src/Contexts/RepresentativeCreationContext.js
+++ src/frontend/src/Contexts/RepresentativeCreationContext.js
@@ -1,6 +1,7 @@
 import React, {createContext, useReducer } from 'react';
 import RepresentativeCreationReducer from '../Reducers/RepresentativeCreationReducer';
-import { hideRepCreationFormModal, showRepCreationFormModal, updateRepCreationFormModal } from '../Actions/RepresentativeCreationActions';
+import { hideRepCreationFormModal, showRepCreationFormModal, updateRepCreationFormModal, updateForm, setResponse } from '../Actions/RepresentativeCreationActions';
+import UserHandler from '../Handlers/UserHandler';
 
 export const RepresentativeCreationContext = createContext();
 
@@ -11,13 +12,38 @@ const RepresentativeCreationContextProvider = ({children}) => {
       repCreationFormModal: {
           title: '',
           description: ''
-      }
+      },
+      form: {
+        firstname: '',
+        name: '',
+        email: '',
+        phone: ''
+      },
+      response: null
     }
   );
 
+  const setField = (e) => {
+    e.persist();
+    dispatch(updateForm(e));
+  }
+
+  const sendForm = (e) => {
+    e.preventDefault();
+
+    UserHandler.create(state.form.firstname, state.form.name, state.form.email, state.form.phone).then((response) => {            
+      dispatch(setResponse(response));
+      // dispatch(hideRepCreationFormModal());
+      return;
+    }).catch((err) => {
+      dispatch(setResponse(err));
+      return;
+    });
+  }
+
   return (
     <div>
-      <RepresentativeCreationContext.Provider value={{ ...state, dispatch, hideRepCreationFormModal, showRepCreationFormModal, updateRepCreationFormModal }} >
+      <RepresentativeCreationContext.Provider value={{ ...state, dispatch, hideRepCreationFormModal, showRepCreationFormModal, updateRepCreationFormModal,  setField, sendForm }} >
         {children}
       </RepresentativeCreationContext.Provider>
     </div>
