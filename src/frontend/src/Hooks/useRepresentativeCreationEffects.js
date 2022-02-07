a8f5cf1761e81654b45b88e627f4a7646a35e71f
Alicja Konopka <alicja.konopka@wp.pl>
2022-02-07 15:39:32 +0100

    fetching clients from database before rendering select list

--- src/frontend/src/Hooks/useRepresentativeCreationEffects.js
+++ src/frontend/src/Hooks/useRepresentativeCreationEffects.js
@@ -1,12 +1,23 @@
 import React, { useEffect } from 'react';
 import RepresentativeCreationFormModal from '../Components/RepresentativeCreation/RepresentativeCreationFormModal';
+import ClientHandler from '../Handlers/ClientHandler';
 
 const useRepresentativeCreationEffects = (props) => {
-  const {repCreationFormModalVisible, dispatch, updateRepCreationFormModal, response, form, client, location } = props;
+  const { repCreationFormModalVisible, dispatch, updateRepCreationFormModal, response, form, client, location, clients, setClients } = props;
 
   useEffect(() => {  
     dispatch(updateRepCreationFormModal({title:'', description: <RepresentativeCreationFormModal {...props}></RepresentativeCreationFormModal>}))    
-  }, [repCreationFormModalVisible, response, form, client, location]);
+  }, [repCreationFormModalVisible, response, form, client, location, clients]);
+
+  useEffect(() => {
+    ClientHandler.getClients().then(response => {
+      let sorted = response.resources.sort((a, b) => {
+          return (a.nazwa) > (b.nazwa) ? 1 : ( (a.nazwa) < (b.nazwa) ? -1 : 0 );
+      });
+      dispatch(setClients(sorted));
+    });
+  }, []);
+
 }
 
 export default useRepresentativeCreationEffects;
