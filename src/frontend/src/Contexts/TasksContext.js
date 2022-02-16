6ac3c239458b22d87479abfe9d4a58f2d9d9f38a
Alicja Konopka <alicja.konopka@wp.pl>
2022-02-16 10:00:20 +0100

    splitting todo into two colmns by task error type

--- src/frontend/src/Contexts/TasksContext.js
+++ src/frontend/src/Contexts/TasksContext.js
@@ -2,12 +2,16 @@ import React, {createContext, useState } from 'react';
 import TaskHandler from '../Handlers/TaskHandler';
 import { Row, Col } from '../Components/bootstrap';
 import TaskItem from '../Components/Tasks/TaskItem';
+import appConfig from '../Config/appConfig.json';
+import _ from "lodash";
 
 export const TasksContext = createContext();
 
 const TasksContextProvider = ({children}) => {
   const [response, setResponse] = useState(null);
   const [tasks, setTasks] = useState([]);
+  const [tasksLeftCol, setTasksLeftCol] = useState([]);
+  const [tasksRightCol, setTasksRightCol] = useState([]);
   const [pickedTask, setPickedTask] = useState(null);
   const [closeDisabledId, setCloseDisabledId] = useState(null);
   const [taskStarted, setTaskStarted] = useState(false);
@@ -24,10 +28,30 @@ const TasksContextProvider = ({children}) => {
     TaskHandler.getTasks( general ? true : false).then((response) => {
         console.log(response);
         setResponse(response);
-        setTasks(response.resources);
-        if(response.resources.length > 0) {
-            setPickedTask(response.resources[0]);
+        let tasks = response.resources;
+
+        if(general) {
+          let errorTypesLeftCol = [93, 36];
+
+          let tasksLeftCol = tasks.filter(task => errorTypesLeftCol.includes(task.id_uslugi));
+          let tasksRightCol = tasks.filter(task => !tasksLeftCol.includes(task));
+
+          setTasksLeftCol(tasksLeftCol);
+          setTasksRightCol(tasksRightCol);
+
+          if(tasksLeftCol.length > 0) {
+            setPickedTask(tasksLeftCol[0]);
+          } else if(tasksRightCol.length > 0) {
+            setPickedTask(tasksRightCol[0]);
+          }
+        } else {          
+          setTasks(tasks);
+
+          if(tasks.length > 0) {
+            setPickedTask(tasks[0]);
+          }
         }
+        
         setCloseDisabledId(null);
     }).catch((err) => {
         setResponse(err);
@@ -66,16 +90,52 @@ const closeTaskPreview = () => {
     setTaskPreviewVisible(false);
 }
 
-const buildTaskRadios = (props) => {
-    return tasks.map((task, key) => {
-      return (
-        <Row key={key}>
-            <Col xs="12">
-                <TaskItem {...props} key={key} task={task}></TaskItem>
-            </Col>
-        </Row>        
-        );
-    });
+const buildTaskRadios = (props) => { 
+  if(props.match.params.general) {
+    return buildTaskGeneralRadios(props);
+  }
+
+  return tasks.map((task, key) => {
+    return (
+      <Row key={key}>
+        <Col xs="12">
+          <TaskItem {...props} key={key} task={task}></TaskItem>
+        </Col>
+      </Row>        
+    );
+  });
+}
+
+const buildTaskGeneralRadios = (props) => { 
+  let header = 
+    <Row key={0} className="margin-top-default margin-bottom-default">
+      <Col xs="6" className="text-center">
+        Zadania typu "Z" / "W"
+      </Col> 
+      <Col xs="6" className="text-center">
+        Zadania pozostałych typów
+      </Col>
+    </Row>  
+    
+  let rows = _.times(Math.max(tasksLeftCol.length, tasksRightCol.length), (key) => (
+    <Row key={key}>
+      <Col xs="6">
+        { tasksLeftCol[key] &&
+          <TaskItem {...props} key={2*key+1} task={tasksLeftCol[key]}></TaskItem>
+        }
+      </Col>
+      <Col xs="6">
+        { tasksRightCol[key] &&
+          <TaskItem {...props} key={2*key+2} task={tasksRightCol[key]}></TaskItem>
+        }
+      </Col>
+    </Row>  
+  ));
+
+  return <>
+    { header } 
+    { rows }
+  </>
 }
 
 return (
