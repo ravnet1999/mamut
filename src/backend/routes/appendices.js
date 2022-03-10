4d20a6e26a9f037e7bbd0d12e6e97727ae075dcd
Alicja Konopka <alicja.konopka@wp.pl>
2022-03-10 20:38:40 +0100

    determines file outside of appendix service create function

--- src/backend/routes/appendices.js
+++ src/backend/routes/appendices.js
@@ -3,17 +3,34 @@ const router = express.Router();
 const authMiddleware = require('../middleware/auth');
 const response = require('../src/response');
 const appendixService = require('../src/service/AppendixService');
+const multiparty = require('multiparty');
 
 router.post('/:taskId', [authMiddleware], (req, res, next) => {
   let taskId = req.params.taskId;
 
-  appendixService.create(taskId, req, res).then((result) => {
-      response(res, false, ['Pomyślnie utworzono nowy załącznik.'], result.resources);
-      return;
-  }).catch((err) => {
-      response(res, true, ['Wystąpił błąd poczas próby utworzenia nowego załącznika.', JSON.stringify(err)], []);
-      return;
-  })
+  var form = new multiparty.Form({maxFieldsSize: 2097152000});
+
+  form.on('error', function(err) {
+    response(res, true, ['Wystąpił błąd poczas próby utworzenia nowego załącznika.', JSON.stringify(err)], []);
+    return;
+  });
+    
+  form.parse(req, function(err, fields, files) {
+    if(files) {
+      let file = Object.values(files)[0][0];
+
+      appendixService.create(taskId, file).then((result) => {
+          response(res, false, ['Pomyślnie utworzono nowy załącznik.'], result.resources);
+          return;
+      }).catch((err) => {
+          response(res, true, ['Wystąpił błąd poczas próby utworzenia nowego załącznika.', JSON.stringify(err)], []);
+          return;
+      })
+    } else {
+      response(res, true, ['Wystąpił błąd poczas próby utworzenia nowego załącznika.'], []);
+      return;  
+    }
+  });
 });
 
 module.exports = router;
