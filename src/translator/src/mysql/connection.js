e0261cce4635461d4292fa01a8fee0719062277d
Alicja Konopka <alicja.konopka@wp.pl>
2022-03-10 15:24:01 +0100

    uses mysql2 npm package instead of mysql in translator container

--- src/translator/src/mysql/connection.js
+++ src/translator/src/mysql/connection.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql');
+const mysql2 = require('mysql2');
 const dbConfig = require('../../config/database.json');
 
 dbConfig.connectionLimit = 10;
 dbConfig.charset = 'utf8'
 
 // const connection = mysql.createConnection(dbConfig);
-const connection = mysql.createPool(dbConfig);
+const connection = mysql2.createPool(dbConfig);
 
 module.exports = connection;
