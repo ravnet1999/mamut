a2572563559e0000fa5ce60004d93a17222504a7
Alicja Konopka <alicja.konopka@wp.pl>
2022-03-10 20:38:40 +0100

    determines file outside of appendix service create function

--- src/backend/src/service/AppendixService.js
+++ src/backend/src/service/AppendixService.js
@@ -1,6 +1,5 @@
 const appConfig = require('../../config/appConfig.json');
 
-const multiparty = require('multiparty');
 const fs = require('fs');
 
 const axios = require('axios');
@@ -10,48 +9,41 @@ var FormData = require('form-data');
 const parseResponse = require('../ResponseParser');
 
 class AppendixService {
-  create = (taskId, req, res) => {
-    var form = new multiparty.Form({maxFieldsSize: 2097152000});
+  create = (taskId, file) => {
+    return new Promise((resolve, reject) => { 
+      fs.createReadStream(file.path).pipe(concat({ encoding: 'buffer' }, function (data) {
+        var formData = new FormData();
+        formData.append("filename", file.originalFilename);
+        formData.append("contentType", file.headers["content-type"]);
+        formData.append("size", file.size);
+        formData.append("data", data);
 
-    return new Promise((resolve, reject) => {  
-      form.on('error', function(err) {
-        return reject(err);
-      });
-
-      form.parse(req, function(err, fields, files) {
-        if(files) {
-          let file = Object.values(files)[0][0];
-
-          fs.createReadStream(file.path).pipe(concat({ encoding: 'buffer' }, function (data) {
-            var formData = new FormData();
-            formData.append("filename", file.originalFilename);
-            formData.append("contentType", file.headers["content-type"]);
-            formData.append("size", file.size);
-            formData.append("data", data);
-
-            axios({
-              method: 'post',
-              url: `${appConfig.URLs.translator}/appendices/${taskId}`,
-              data: formData,
-              maxContentLength: Infinity,
-              maxBodyLength: Infinity,
-              // headers: {'Content-Type': 'multipart/form-data;boundary=' + formData.getBoundary()}
-              headers: formData.getHeaders()
-            }).then((response) => {
-              parseResponse(response).then((result) => {
-                return resolve({ resources: result });      
-              }).catch((err) => {
-                return reject(err);                  
-              });
-            }).catch((err) => {
-              return reject({
-                  message: 'Wystąpił problem z połączeniem z translatorem.'
-              });
-            });
-          }))
-        }
-      });        
-    });
+        axios({
+          method: 'post',
+          url: `${appConfig.URLs.translator}/appendices/${taskId}`,
+          data: formData,
+          maxContentLength: Infinity,
+          maxBodyLength: Infinity,
+          // headers: {'Content-Type': 'multipart/form-data;boundary=' + formData.getBoundary()}
+          headers: formData.getHeaders()
+        })
+        // axios.post(`${appConfig.URLs.translator}/appendices/${taskId}`, formData, {
+        //   headers: formData.getHeaders()
+        // })
+        .then((response) => {
+          parseResponse(response).then((result) => {
+            return resolve({ resources: result });      
+          }).catch((err) => {
+            return reject(err);                  
+          });
+        }).catch((err) => {
+          console.log(err)
+          return reject({
+              message: 'Wystąpił problem z połączeniem z translatorem.'
+          });
+        });
+      }))
+    })
   }
 }
 
