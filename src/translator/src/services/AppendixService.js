4ee0d26507d3e6916ea11923b2096c98067aa4ea
Alicja Konopka <alicja.konopka@wp.pl>
2022-03-24 14:49:06 +0100

    value for field "godzina" in appendices table passed directly to sql query for inserting appendices

--- src/translator/src/services/AppendixService.js
+++ src/translator/src/services/AppendixService.js
@@ -21,8 +21,8 @@ class AppendixService extends Service {
       return new Promise((resolve, reject) => {
         // connection.query('INSERT INTO `' + this.tableName + '`(id_zgloszenia, nazwa, nazwa_oryginalna, sciezka, rozmiar, typ_mime, zawartosc) VALUES (?,?,?,?,?,?,?)', 
         // [taskId, file.filename[0], file.originalFilename[0], file.path[0], parseInt(file.size[0]), file.contentType[0], file.data[0]], (err, results, fields) => {
-        connection.query('INSERT INTO `' + this.tableName + '`(id_zgloszenia, nazwa, nazwa_oryginalna, sciezka, rozmiar, typ_mime, godzina) VALUES (?,?,?,?,?,?,?)', 
-          [taskId, file.filename, file.originalFilename, file.path, parseInt(file.size), file.contentType, 'NOW()'], (err, results, fields) => {
+        connection.query('INSERT INTO `' + this.tableName + '`(id_zgloszenia, nazwa, nazwa_oryginalna, sciezka, rozmiar, typ_mime, godzina) VALUES (?,?,?,?,?,?,NOW())', 
+          [taskId, file.filename, file.originalFilename, file.path, parseInt(file.size), file.contentType], (err, results, fields) => {
             if(err) {            
               reject(err);
               return;
