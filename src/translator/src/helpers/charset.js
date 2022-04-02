bf41425b5f47eaccf218cc7ca7463cc2583b97d2
Alicja Konopka <alicja.konopka@wp.pl>
2022-04-02 14:29:06 +0200

    disables charset changes in translator due to database charset change

--- src/translator/src/helpers/charset.js
+++ src/translator/src/helpers/charset.js
@@ -22,32 +22,32 @@ class Charset {
     }
 
     translateIn = (obj) => {
-        for(let key in obj) {
-            if(obj.hasOwnProperty(key) && typeof obj[key] == 'string' && obj[key].length > 0) {
-                obj[key] = obj[key].replace(/¹|¥|æ|Æ|ê|Ê|³|£|ñ|Ñ|œ|Œ|¿|¯|Ÿ|/g, (letter) => {
-                    for(let dictLetter in this.dictionary) {
-                        let fittingLetters = this.dictionary[dictLetter].filter((foreignLetter) => {
-                            return foreignLetter == letter;
-                        });
-                        if(fittingLetters.length > 0) {
-                            return dictLetter;
-                        }
-                    }
-                });
-            }
-        }
+        // for(let key in obj) {
+        //     if(obj.hasOwnProperty(key) && typeof obj[key] == 'string' && obj[key].length > 0) {
+        //         obj[key] = obj[key].replace(/¹|¥|æ|Æ|ê|Ê|³|£|ñ|Ñ|œ|Œ|¿|¯|Ÿ|/g, (letter) => {
+        //             for(let dictLetter in this.dictionary) {
+        //                 let fittingLetters = this.dictionary[dictLetter].filter((foreignLetter) => {
+        //                     return foreignLetter == letter;
+        //                 });
+        //                 if(fittingLetters.length > 0) {
+        //                     return dictLetter;
+        //                 }
+        //             }
+        //         });
+        //     }
+        // }
 
         return obj;
     }
 
     translateOut = (obj) => {
-        for(let key in obj) {
-            if(obj.hasOwnProperty(key) && typeof obj[key] == 'string' && obj[key].length > 0) {
-                obj[key] = obj[key].replace(/ą|Ą|ć|Ć|ę|Ę|ł|Ł|ń|Ń|ś|Ś|ż|Ż|ź|Ź/g, (letter) => {
-                    return this.dictionary[letter][0];
-                });
-            }
-        }
+        // for(let key in obj) {
+        //     if(obj.hasOwnProperty(key) && typeof obj[key] == 'string' && obj[key].length > 0) {
+        //         obj[key] = obj[key].replace(/ą|Ą|ć|Ć|ę|Ę|ł|Ł|ń|Ń|ś|Ś|ż|Ż|ź|Ź/g, (letter) => {
+        //             return this.dictionary[letter][0];
+        //         });
+        //     }
+        // }
 
         return obj;
     }
