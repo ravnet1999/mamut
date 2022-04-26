1dd0ba0625993bb9e862cc8d2fa789d7c774b9a0
Alicja Konopka <alicja.konopka@wp.pl>
2022-04-26 15:50:50 +0200

    task appendices tags context

--- src/frontend/src/Contexts/TaskAppendicesTagsContext.js
+++ src/frontend/src/Contexts/TaskAppendicesTagsContext.js
@@ -9,6 +9,11 @@ const TaskAppendicesTagsContextProvider = ({children}) => {
   const [tagToCreateFocus, setTagToCreateFocus] = useState(false);  
   const [tagsConfirmed, setTagsConfirmed] = useState([]);
 
+  const [tag, setTag] = useState(null);
+  const [tags, setTags] = useState([]);
+  const [tagKey, setTagKey] = useState(null);
+  const [tagsFocus, setTagsFocus] = useState([]);
+
   const onTagToCreateChange = tagName => {
     setTagToCreate(tagName);
   }
@@ -22,6 +27,88 @@ const TaskAppendicesTagsContextProvider = ({children}) => {
   const onTagConfirmedRemove = tagName => {
     setTagsConfirmed(tagsConfirmed.filter(name => name != tagName));
   }
+
+  const afterTagCreated = (appendixId) => {
+    let tagsFocusUpdated = [];
+    tagsFocusUpdated[appendixId] = true;
+    setTagsFocus(tagsFocusUpdated);
+
+    setTag(null);
+    setTagKey(Math.random().toString(36)); 
+
+    let tagsUpdated = tags.filter(tag => tag.appendixId != appendixId);
+    setTags([...tagsUpdated]);
+  }
+
+  const onTagCreate = async (appendix, updateAppendicesOnTagCreate, setResponse) => {    
+    let appendixId = appendix.id;
+    let tagsFiltered = tags.filter(tag => tag.appendixId == appendixId);
+
+    if(!tagsFiltered.length || tagsFiltered[0].name.length < 3) {
+      setResponse({
+        error: true,
+        messages: ['Tag musi mieć co najmniej 3 znaki.']
+      }); 
+      return;   
+    } 
+
+    let tagName = tagsFiltered[0].name;
+
+    if(Object.values(appendix.tagi).includes(tagName)) {
+      setResponse({
+        error: true,
+        messages: [`Tag "${tagName}" już istnieje.`]
+      }); 
+      updateAppendicesOnTagCreate(appendixId, tagName);
+      return;   
+    }
+
+    try {
+      updateAppendicesOnTagCreate(appendixId, tagName);  
+      
+      setResponse({
+        error: false,
+        messages: ['Pomyślnie dodano tag do załącznika.']
+      });  
+    } catch(err) {
+      console.log(err);
+      setResponse(err);
+    } finally {
+      afterTagCreated(appendixId);
+    }
+  }
+
+  const onTagChange = (appendix, tagName, setResponse) => {
+    let appendixId = appendix.id;
+
+    let tagsUpdated = tags.filter(tag => tag.appendixId != appendixId);
+    tagsUpdated.push({appendixId, name:tagName});
+    setTags([...tagsUpdated]);
+  }
+
+  const onTagRemove = async (appendix, tagId, updateAppendicesOnTagRemove, setResponse) => {    
+    if(Object.entries(appendix.tagi).length == 1) { 
+      setResponse({
+        error: true,
+        messages: ['Nie można usunąć jedynego tagu do załącznika.']
+      }); 
+      return;
+    }
+    
+    let appendixId = appendix.id;
+
+    try {
+      updateAppendicesOnTagRemove(appendixId, tagId);        
+      
+      setResponse({
+        error: false,
+        messages: ['Pomyślnie usunięto tag do załącznika.']
+      });                  
+    } catch(err) {
+      console.log(err);
+      setResponse(err);
+    }; 
+  }
   
   return (
     <div>
@@ -31,9 +118,19 @@ const TaskAppendicesTagsContextProvider = ({children}) => {
         tagToCreateKey, setTagToCreateKey,
         tagToCreateFocus, setTagToCreateFocus, 
         tagsConfirmed, setTagsConfirmed,
+
+        tag, setTag,
+        tags, setTags,
+        tagKey, setTagKey,
+        tagsFocus, setTagsFocus,
+
         onTagToCreateChange, 
         afterTagToCreateConfirmed,
-        onTagConfirmedRemove         
+        onTagConfirmedRemove,
+        
+        onTagCreate,
+        onTagChange,
+        onTagRemove
       }}>
         {children}
       </TaskAppendicesTagsContext.Provider>
