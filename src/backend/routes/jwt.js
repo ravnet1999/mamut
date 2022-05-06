93621cd2925ed4819a93024b75bc98a65362864c
Alicja Konopka <alicja.konopka@wp.pl>
2022-05-06 12:10:16 +0200

    Revert "disables registration using jwt"
    This reverts commit 08ef7d156178a45d5835e28764ac00c38c178add.

--- src/backend/routes/jwt.js
+++ src/backend/routes/jwt.js
@@ -8,51 +8,51 @@ const jwtConfig = require('../config/jwtConfig.json');
 const JwtUser = require("../src/models/JwtUser");
 
 
-// router.post('/register', [], async (req, res, next) => {
-//   // Our register logic starts here
-//   try {
-//     // Get user input
-//     const { login, password } = req.body;
+router.post('/register', [], async (req, res, next) => {
+  // Our register logic starts here
+  try {
+    // Get user input
+    const { login, password } = req.body;
 
-//     // Validate user input
-//     if (!(login && password)) {      
-//       return response(res, true, ['Wymagany login i hasło.'], []);
-//     }
+    // Validate user input
+    if (!(login && password)) {      
+      return response(res, true, ['Wymagany login i hasło.'], []);
+    }
 
-//     // check if user already exist
-//     // Validate if user exist in our database
-//     const oldUser = await JwtUser.findOne({ login });
+    // check if user already exist
+    // Validate if user exist in our database
+    const oldUser = await JwtUser.findOne({ login });
 
-//     if (oldUser) {      
-//       return response(res, true, ['Taki użytkownik już istnieje.'], []);
-//     }
+    if (oldUser) {      
+      return response(res, true, ['Taki użytkownik już istnieje.'], []);
+    }
 
-//     //Encrypt user password
-//     encryptedPassword = await bcrypt.hash(password, 10);
+    //Encrypt user password
+    encryptedPassword = await bcrypt.hash(password, 10);
 
-//     // Create user in our database
-//     const jwtUser = await JwtUser.create({
-//       login: login.toLowerCase(), // sanitize: convert email to lowercase
-//       password: encryptedPassword,
-//     });
+    // Create user in our database
+    const jwtUser = await JwtUser.create({
+      login: login.toLowerCase(), // sanitize: convert email to lowercase
+      password: encryptedPassword,
+    });
 
-//     // Create token
-//     const token = jwt.sign(
-//       { user_id: jwtUser._id, login },
-//       jwtConfig.JWT_TOKEN_KEY,
-//       {
-//         expiresIn: "2h",
-//       }
-//     );
-//     // save user token
-//     jwtUser.token = token;
+    // Create token
+    const token = jwt.sign(
+      { user_id: jwtUser._id, login },
+      jwtConfig.JWT_TOKEN_KEY,
+      {
+        expiresIn: "2h",
+      }
+    );
+    // save user token
+    jwtUser.token = token;
 
-//     // return new user
-//     return response(res, false, ['Pomyślnie zarejestrowano.'], [ jwtUser ]);
-//   } catch (err) {
-//     return response(res, true, ['Wystąpił problem z rejestracją.', JSON.stringify(err)], []);
-//   }    
-// });
+    // return new user
+    return response(res, false, ['Pomyślnie zarejestrowano.'], [ jwtUser ]);
+  } catch (err) {
+    return response(res, true, ['Wystąpił problem z rejestracją.', JSON.stringify(err)], []);
+  }    
+});
 
 router.post('/login', [], async (req, res, next) => {
   // Our login logic starts here
